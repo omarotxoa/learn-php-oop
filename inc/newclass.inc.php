74581e6ecc0c71e69331cf84ce99242a7af2b64f
Omar Ochoa <omar@ochoawebdesign.com>
2023-07-19 17:43:28 -0700

    Basic class dumping a string onto the page from a public property

---
+++ inc/newclass.inc.php
@@ -0,0 +1,12 @@
+<?php 
+
+class NewClass {
+    // Properties and Methods goes here
+    // Properties are similar to variables and Methods are similar to functions
+    public $info = "This is some info";
+}
+
+$object = new NewClass;
+print_r($object);
+echo "<br>";
+var_dump($object);
